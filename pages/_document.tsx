6636e055e56cb6d70bd47b0905038bb22fcd87ae
patibhacoder <pratibharawat322@gmail.com>
2024-08-05 21:43:43 +0530

    updated button styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,10 +16,8 @@ class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="description" content="Better.com - Your journey to better mortgage starts here." />
           <meta name="keywords" content="mortgage, home loans, better.com" />
-          <link rel="icon" href="/favicon.ico" />
-
-          {/* Tailwind CSS import */}
-          <link href="/styles/globals.css" rel="stylesheet" />
+      
+          <link href="globals.css" rel="stylesheet" />
 
           {/* Additional fonts or styles can be added here */}
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
